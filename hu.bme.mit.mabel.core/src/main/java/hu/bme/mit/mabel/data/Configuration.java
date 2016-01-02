d0b9a0b72b138cbfae87cb6f2c6244b6d639f7d8
Gabor Szarnyas <szarnyasg@gmail.com>
2016-01-02 14:37:56 +0100

    Set run option as required

--- hu.bme.mit.mabel.core/src/main/java/hu/bme/mit/mabel/data/Configuration.java
+++ hu.bme.mit.mabel.core/src/main/java/hu/bme/mit/mabel/data/Configuration.java
@@ -5,40 +5,40 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 /**
- * The input parameters of a benchmark workflow. 
+ * The input parameters of a benchmark workflow.
  */
 public class Configuration {
-	
-	@Option(name = "-runs", usage = "number of runs")
+
+	@Option(required = true, name = "-runs", usage = "number of runs")
 	protected int runs;
-	
+
 	@Option(name = "-verbose", usage = "show verbose output")
 	protected boolean verbose;
-	
+
 	public Configuration(final int runs, final boolean verbose) {
 		super();
 		this.runs = runs;
 		this.verbose = verbose;
 	}
-	
+
 	protected Configuration() {
 	}
 
 	/**
-	 * Parses a {@link Configuration} from the given command-line arguments. 
+	 * Parses a {@link Configuration} from the given command-line arguments.
 	 */
-	public static Configuration parse(String[] args) throws CmdLineException {
-		Configuration configuration = new Configuration();
+	public static Configuration parse(final String[] args) throws CmdLineException {
+		final Configuration configuration = new Configuration();
 		new CmdLineParser(configuration).parseArgument(args);
 		return configuration;
 	}
-	
+
 	public int getRuns() {
 		return runs;
 	}
-	
+
 	public boolean isVerbose() {
 		return verbose;
-	}	
-	
+	}
+
 }
